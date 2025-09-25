fefc8b3a22eeef74f8350f86e16a14af873b335a
max-laptop <maximilian.merzdorf@gmail.com>
2025-09-25 11:54:47 +0200

    updated for lapply use

--- reproject_rasters.R
+++ reproject_rasters.R
@@ -1,10 +1,14 @@
-# reproject orthomosaics to epsg:32632 (UTM 32 N)
-repro_32n <- function(rastfile){
-  myrast <- terra::rast(paste0("C:/EAGLE/InnoLab/data/_raster/",rastfile))
-  myrast <- terra::project(myrast, "epsg:32632")
-  terra::writeRaster(myrast, filename=paste0("C:/EAGLE/InnoLab/data/_raster/", substr(rastfile, 1, 11), "_DEM_repro.tif"))
+library(terra)
+library(tictoc)
+
+repro <- function(rfile, crs){
+  tic("start")
+  r <- terra::rast(rfile)
+  r <- terra::project(r, crs)
+  terra::writeRaster(r, rfile, overwrite = TRUE)
+  toc()
 }
 
-filesls <- list.files("./data/_raster/", pattern="DEM.tif_clipped.tif")
-
-lapply(filesls, repro_32n)
+rfiles <- list.files("./data/_raster/original/", pattern = ".tif$", full.names = T)
+rfiles
+lapply(rfiles, repro, "epsg:32632")
