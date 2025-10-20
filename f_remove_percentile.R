b2e1acf33c242f30c0a7bfe83538e872891be27a
max-laptop <maximilian.merzdorf@gmail.com>
2025-10-20 16:10:06 +0200

    added terra dependency specifications

--- f_remove_percentile.R
+++ f_remove_percentile.R
@@ -1,4 +1,4 @@
 remove_percentile <- function(r, percentile){
-  values(r)[values(r) >= quantile(values(r), probs = percentile, na.rm = T)] <- NA
+  terra::values(r)[terra::values(r) >= quantile(values(r), probs = percentile, na.rm = T)] <- NA
   return(r)
 }
--- f_rescale_to_8bit.R
+++ f_rescale_to_8bit.R
@@ -1,6 +1,6 @@
 rescale_to_8bit <- function(r){
-  r_min <- min(global(r, "min", na.rm = TRUE)[[1]])
-  r_max <- max(global(r, "max", na.rm = TRUE)[[1]])
+  r_min <- min(terra::global(r, "min", na.rm = TRUE)[[1]])
+  r_max <- max(terra::global(r, "max", na.rm = TRUE)[[1]])
   
   img_scaled <- round( (r - r_min) / (r_max - r_min) * 255)
   return(r)
