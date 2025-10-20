0f97915f870b496e584036bf6213444f1e366b9d
max-laptop <maximilian.merzdorf@gmail.com>
2025-10-20 16:38:37 +0200

    fixed return

--- f_rescale_to_8bit.R
+++ f_rescale_to_8bit.R
@@ -2,6 +2,6 @@ rescale_to_8bit <- function(r){
   r_min <- min(terra::global(r, "min", na.rm = TRUE)[[1]])
   r_max <- max(terra::global(r, "max", na.rm = TRUE)[[1]])
   
-  img_scaled <- round( (r - r_min) / (r_max - r_min) * 255)
+  r <- round( (r - r_min) / (r_max - r_min) * 255)
   return(r)
 }
