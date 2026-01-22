556ad8ae94ec6486d534dfc5b7a79e2a1a865b64
max-laptop <maximilian.merzdorf@gmail.com>
2026-01-22 12:58:37 +0100

    first version

---
+++ results_graphics.R
@@ -0,0 +1,9 @@
+# First: cread all the result files and check
+uas_slopes <- read.csv("./results/ACTUAL_RESULTS/UAS_slopes_FINAL.csv")
+uas_species_vars <- read.csv("./results/ACTUAL_RESULTS/UAS_species_vars_pearson.csv")
+uas_structure_vars <- read.csv("./results/ACTUAL_RESULTS/UAS_structure_vars_pearson.csv")
+
+planet_slopes <- read.csv("./results/ACTUAL_RESULTS/Planet_slopes_FINAL.csv")
+planet_species_vars <- read.csv("./results/ACTUAL_RESULTS/Planet_species_vars_pearson.csv")
+planet_structure_vars <- read.csv("./results/ACTUAL_RESULTS/Planet_structure_vars_pearson.csv")
+
