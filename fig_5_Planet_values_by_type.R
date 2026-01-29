832a2ca3b248964154e782e1216f633768ef1294
max-laptop <maximilian.merzdorf@gmail.com>
2026-01-30 00:07:34 +0100

    figure fixes

---
+++ fig_5_Planet_values_by_type.R
@@ -0,0 +1,38 @@
+planet_predictors <- read.csv("./results/Planet_texture_metric_results.csv")
+mgmt_type <- read.table("./data/_tables/_analysisready_env_params.csv")
+
+# get site management type
+mgmt_type <- mgmt_type %>%
+  filter(!siteID %in% c(16:20, 24:27)) %>%
+  filter(Col_run == 1) %>%
+  select(c(siteID, Type)) %>%
+  mutate(site = paste0("site", siteID)) %>%
+  select(-siteID)
+  
+u <- planet_predictors %>%
+  filter(!site %in% c("site16", "site17", "site18", "site19", "site20",
+                      "site24", "site25", "site26", "site27")) %>%
+  left_join(mgmt_type, by = "site") %>%
+  select(c(starts_with("B1"), "SSD", "Type")) %>%
+  rename_with(~ gsub("B1_", "", .x, fixed = TRUE)) %>%
+  pivot_longer(cols = -c(Type)) %>%
+  mutate(Type = factor(case_when(
+    Type == "int_prox" ~ "int",
+    Type == "int_cit" ~ "int",
+    Type == "ext_prox" ~ "ext",
+    .default = Type
+  ), levels = c("int", "ext", "semi_nat")))
+
+p <- ggplot(u, aes(x = name, y = value, fill = as.factor(Type))) +
+  geom_boxplot() +
+  scale_fill_viridis_d() +
+  labs(title = "PlanetScope predictor values grouped by site management type") +
+  guides(fill = guide_legend(title = "Type")) +
+  scale_x_discrete(guide = guide_axis(n.dodge=2)) +
+  ylim(c(0,30)) + 
+  xlab("Predictor") +
+  ylab("Value") +
+  theme_light()
+p
+ggsave("./images/5_Planet_values_by_type.png", plot = p, width = 3000, height = 1500, units = "px", dpi = 300)
+
